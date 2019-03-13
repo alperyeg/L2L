ca9a7b95286d47b8a565142ebe18af21a2568ce1
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2019-03-13 16:29:55 +0100

    added the enkf optimizer, preliminary version

---
+++ l2l/optimizers/kalmanfilter/optimizer.py
@@ -0,0 +1,184 @@
+import logging
+from collections import namedtuple
+from l2l import get_grouped_dict
+from l2l.utils.tools import cartesian_product
+from .updateEnKF import update_enknf
+
+import numpy as np
+
+from sklearn.datasets import load_digits
+
+from l2l import dict_to_list
+from l2l import list_to_dict
+from l2l.optimizers.optimizer import Optimizer
+
+logger = logging.getLogger("optimizers.kalmanfilter")
+
+EnsembleKalmanFilterParameters = namedtuple(
+    'EnsembleKalmanFilter', ['noise', 'gamma', 'tol',
+                             'maxit', 'stopping_crit', 'n_iteration',
+                             'pop_size']
+)
+
+
+EnsembleKalmanFilterParameters.__doc__ = """
+:param noise
+:param gamma
+:param tol
+:param maxit
+:param stopping_crit
+:param n_iteration
+:param pop_size
+"""
+
+
+class EnsembleKalmanFilter(Optimizer):
+    """
+    Class for an Ensemble Kalman Filter optimizer
+    """
+    def __init__(self, traj,
+                 optimizee_create_individual,
+                 optimizee_fitness_weights,
+                 parameters,
+                 optimizee_bounding_func=None):
+        super().__init__(traj,
+                         optimizee_create_individual=optimizee_create_individual,
+                         optimizee_fitness_weights=optimizee_fitness_weights,
+                         parameters=parameters,
+                         optimizee_bounding_func=optimizee_bounding_func)
+
+        self.optimizee_bounding_func = optimizee_bounding_func
+        self.optimizee_create_individual = optimizee_create_individual
+        self.optimizee_fitness_weights = optimizee_fitness_weights
+        self.parameters = parameters
+
+        traj.f_add_parameter('gamma', parameters.gamma, comment='Noise level')
+        # traj.f_add_parameter('p', parameters.p, comment='Exact solution')
+        traj.f_add_parameter('noise', parameters.noise,
+                             comment='Multivariate noise distribution')
+        traj.f_add_parameter('tol', parameters.tol,
+                             comment='Convergence threshold')
+        traj.f_add_parameter('maxit', parameters.maxit,
+                             comment='Maximum iterations')
+        traj.f_add_parameter('n_iteration', parameters.n_iteration,
+                             comment='Number of iterations to run')
+        traj.f_add_parameter('stopping_crit', parameters.stopping_crit,
+                             comment='Name of stopping criterion')
+
+        _, self.optimizee_individual_dict_spec = dict_to_list(
+            self.optimizee_create_individual(), get_dict_spec=True)
+
+        #: The current generation number
+        self.g = 0
+        #: The population (i.e. list of individuals) to be evaluated at the
+        # next iteration
+        current_eval_pop = [self.optimizee_create_individual() for _ in range(parameters.pop_size)]
+
+        if optimizee_bounding_func is not None:
+            current_eval_pop = [self.optimizee_bounding_func(ind) for ind in current_eval_pop]
+
+        self.eval_pop = current_eval_pop
+        self.eval_pop_asarray = np.array([dict_to_list(x) for x in self.eval_pop])
+
+        self._expand_trajectory(traj)
+
+    def post_process(self, traj, fitnesses_results):
+        """
+        This is the key function of this class. Given a set of :obj:`fitnesses_results`,
+        and the :obj:`traj`, it uses the fitness to decide on the next set of
+        parameters to be evaluated. Then it fills the :attr:`.Optimizer.eval_pop`
+        with the list of parameters it wants evaluated at the next simulation
+        cycle, increments :attr:`.Optimizer.g` and calls :meth:`._expand_trajectory`
+
+        :param  ~l2l.utils.trajectory.Trajectory traj: The trajectory that contains the parameters and the
+            individual that we want to simulate. The individual is accessible using `traj.individual` and parameter e.g.
+            param1 is accessible using `traj.param1`
+
+        :param list fitnesses_results: This is a list of fitness results that contain tuples run index and the fitness.
+            It is of the form `[(run_idx, run), ...]`
+
+        """
+        # NOTE: Always remember to keep the following two lines.
+        self.g += 1
+        n_iter = traj.generation
+        individuals = traj.individuals[n_iter]
+        # works now for pop_size = 1
+        shifts_per_individual = []
+        gamma = traj.gamma
+        for i in individuals:
+            # shifts are the ensembles
+            # weights = i.weights
+            # TODO change to row view
+            ens = np.array(i.shift).T
+            shifts_per_individual.append(ens)
+            ensemble_size = ens.shape[1]
+            if ens.ndim == 1:
+                ens = ens[np.newaxis].T
+            # if weights.ndim == 1:
+            #     weights = weights[np.newaxis]
+            data_input = i.input
+            data_targets = i.targets[np.newaxis].T
+            model = i.model
+            results = update_enknf(data=data_input[0],
+                                   ensemble=ens,
+                                   ensemble_size=ensemble_size,
+                                   moments1=np.mean(ens, axis=1),
+                                   # moments1 = weights.mean(axis=1)
+                                   u_exact=None,
+                                   observations=data_targets[0],
+                                   model=model, noise=traj.noise, p=None,
+                                   gamma=gamma,  tol=traj.tol,
+                                   maxit=traj.maxit,
+                                   stopping_crit=traj.stopping_crit)
+        generation_name = 'generation_{}'.format(self.g)
+        traj.results.generation_params.f_add_result_group(generation_name)
+        generation_result_dict = {
+            'success': []
+        }
+
+        traj.results.generation_params.f_add_result(
+            generation_name + '.algorithm_params', generation_result_dict)
+        # TODO: Set eval_pop to the values of parameters you want to evaluate
+        #  in the next cycle
+        # self.eval_pop = ...
+        self._expand_trajectory(traj)
+
+    def end(self, traj):
+        """
+        Run any code required to clean-up, print final individuals etc.
+
+        :param  ~l2l.utils.trajectory.Trajectory traj: The  trajectory that contains the parameters and the
+            individual that we want to simulate. The individual is accessible using `traj.individual` and parameter e.g.
+            param1 is accessible using `traj.param1`
+
+        """
+        pass
+
+    def _expand_trajectory(self, traj):
+        """
+        Add as many explored runs as individuals that need to be evaluated. Furthermore, add the individuals as explored
+        parameters.
+
+        :param  ~l2l.utils.trajectory.Trajectory traj: The  trajectory that contains the parameters and the
+            individual that we want to simulate. The individual is accessible using `traj.individual` and parameter e.g.
+            param1 is accessible using `traj.param1`
+
+        :return:
+        """
+
+        grouped_params_dict = get_grouped_dict(self.eval_pop)
+        grouped_params_dict = {'individual.' + key: val for key, val in
+                               grouped_params_dict.items()}
+
+        final_params_dict = {'generation': [self.g],
+                             'ind_idx': range(len(self.eval_pop))}
+        final_params_dict.update(grouped_params_dict)
+
+        # We need to convert them to lists or write our own custom IndividualParameter ;-)
+        # Note the second argument to `cartesian_product`: This is for only having the cartesian product
+        # between ``generation x (ind_idx AND individual)``, so that every individual has just one
+        # unique index within a generation.
+        traj.f_expand(cartesian_product(final_params_dict,
+                                        [('ind_idx',) + tuple(
+                                            grouped_params_dict.keys()),
+                                         'generation']))
---
+++ l2l/optimizers/kalmanfilter/updateEnKF.py
@@ -0,0 +1,260 @@
+import numpy as np
+from numpy import sum, sqrt
+from numpy.linalg import norm, inv, solve
+# from moments import moments
+
+
+def update_enknf(data, ensemble, ensemble_size, moments1, u_exact,
+                 observations, model, gamma, p, noise, tol, maxit, stopping_crit):
+    """
+    Ensemble Kalman Filter
+
+    :param ensemble: nd numpy array, contains the calculated ensembles u
+    :param ensemble_size: int, number of ensembles
+    :param moments1: nd numpy array, first moment (mean)
+    :param u_exact: nd numpy array, exact control
+    :param observations: nd numpy array, noisy observation
+    :param G: nd numpy array, Model
+            `G` maps the control (dim n) into the observed data `y` (dim k),
+            inverse of `A` using `numpy.inv`
+    :param  gamma: nd numpy array
+            `noise_level * I` (I is identity matrix)
+    :param p: nd numpy array
+            Exact solution given by :math:`G * u_exact`, where `G` is inverse
+            of a linear elliptic function `L`, it maps the control into the
+            observed data, see section 5.1 of Herty2018
+    :param noise: nd numpy array, Multivariate normal distribution
+    :param tol: float, tolerance for convergence
+    :param maxit: int, maximum number of iteration
+        if the update step does not stop
+    :param stopping_crit: str, stopping criterion,
+            `discrepancy`: checks if the actual misfit is smaller or equal
+            to the noise
+            `relative`: checks if the absolute value between actual and
+            previous misfit is smaller than given tolerance `tol`
+    :return:
+        M: nd numpy array, Misfit
+            Measures the quality of the solution at each iteration,
+            see (Herty2018 eq.30 and eq.31)
+        E: nd numpy array, Deviation of :math:`v^j` from the mean,
+            where the :math:`v^j` is the j-th sample of the approximated
+            distribution of samples, see (Herty2018 eq.28)
+        R: nd numpy array, Deviation of :math:`v^j` from the "true" solution
+            :math:`u^{\dagger}`, see (Herty2018 eq.29)
+        AE: nd numpy array, Deviation of :math:`v^j` under the application of
+            the model `G`, see (Herty2018 eq.28)
+        AR: nd numpy array, Deviation of from the "true" solution under
+            the application of model `G`, see (Herty2018 eq.28)
+    """
+    # Initializations
+    M = []
+    E = []
+    R = []
+    AE = []
+    AR = []
+
+    m1 = moments1
+
+    e = np.zeros_like(ensemble)
+    r = np.zeros_like(ensemble)
+    misfit = np.zeros_like(ensemble)
+
+    norm_uexact2 = None
+    norm_p2 = None
+
+    if u_exact is not None:
+        norm_uexact2 = norm(u_exact) ** 2
+    if p is not None:
+        norm_p2 = norm(p) ** 2
+
+    # get shapes
+    gamma_s, dims = _get_shapes(observations, ensemble)
+
+    if isinstance(gamma, (int, float)):
+        if float(gamma) == 0.:
+            gamma = np.eye(gamma_s)
+    # sqrt_inv_gamma = sqrt(inv(gamma))
+
+    Cpp = None
+    Cup = None
+
+    ensemble = ensemble
+
+    for i in range(maxit):
+        # # update G
+        # G = model.get_output_activation(data, *flatten_to_net_weights(
+        #     model, m1))
+        # G = G[np.newaxis].T
+        g_all = []
+        for l in range(ensemble_size):
+            g_all.append(model.get_output_activation(data,
+                                                     *_flatten_to_net_weights(
+                                                         model,
+                                                         ensemble[:, l])))
+            e[:, l] = ensemble[:, l] - m1
+        g_all = np.array(g_all).T
+        if u_exact is not None:
+            misfit, r = _calculate_misfit(ensemble, misfit, r, g_all, noise)
+        # _convergence(norm_uexact2=norm_uexact2,
+        #              norm_p2=norm_p2,
+        #              sqrt_inv_gamma=sqrt_inv_gamma,
+        #              ensemble_size=ensemble_size, G=G, m1=m1, M=M, E=E, R=R,
+        #              AE=AE, AR=AR, e=e, r=r, misfit=misfit)
+        if stopping_crit == 'discrepancy' and noise > 0:
+            if M[i] <= np.linalg.norm(noise, 2) ** 2:
+                break
+        elif stopping_crit == 'relative':
+            if i >= 1:
+                if np.abs(M[i] - M[i - 1]) < tol:
+                    break
+        else:
+            pass
+            # sc = _stopping_criterion(y=observations, y_hat=G)
+            # if sc <= tol:
+            #     break
+        for d in range(dims):
+            if dims >= 2:
+                g = g_all[d]
+            else:
+                g = g_all
+            # Calculate the covariances
+            Cup = _cov_mat(ensemble, g, ensemble_size)
+            Cpp = _cov_mat(g, g, ensemble_size)
+            for j in range(ensemble_size):
+                # try linalg.lstsq(a,b) if a is not square
+                target = np.zeros(g.shape[0])
+                target[observations] = 1
+                target = target[np.newaxis].T
+                # tmp = solve(Cpp + gamma, target - np.argmax(G, 0))
+                tmp = solve(Cpp + gamma, target - g[:, j])
+                ensemble[:, j] = ensemble[:, j] + (Cup @ tmp)[:, j]
+        m1 = np.mean(ensemble, axis=1)
+    # return M, E, R, AE, AR, Cpp, Cup, m1
+    return ensemble, Cpp, Cup, m1
+
+
+def _convergence(m1, sqrt_inv_gamma,
+                 ensemble_size, G,
+                 M, E, R, AE, AR,
+                 e, r, misfit,
+                 norm_uexact2=None, norm_p2=None):
+    E.append(norm(e) ** 2 / norm(m1) ** 2 / ensemble_size)
+    AE.append(norm(sqrt_inv_gamma @ G @ e) ** 2 / norm(G @ m1) ** 2 / ensemble_size)
+    if norm_uexact2 is not None:
+        R.append((norm(r) ** 2 / norm_uexact2) / ensemble_size)
+        M.append((norm(misfit) ** 2) / ensemble_size)
+    if norm_p2 is not None:
+        AR.append(norm(sqrt_inv_gamma @ G @ r) ** 2 / norm_p2 / ensemble_size)
+    return
+
+
+def _cov_mat(x, y, ensemble_size):
+    """
+    Covariance matrix
+    """
+    x_bar = _get_mean(x)
+    y_bar = _get_mean(y)
+
+    cov = np.zeros((x.shape[0], y.shape[0]))
+    for j in range(ensemble_size):
+        try:
+            cov = cov + np.tensordot((x[:, j] - x_bar), (y[:, j] - y_bar).T, 0)
+        except IndexError:
+            cov = cov + np.tensordot((x[:, j] - x_bar), (y - y_bar).T, 0)
+    cov /= ensemble_size
+    return cov
+
+
+def _get_mean(x):
+    """
+    Depending on the shape returns the correct mean
+    """
+    if x.shape[1] == 1:
+        return np.mean(x)
+    return np.mean(x, axis=1)
+
+
+def _get_shapes(observations, ensemble):
+    """
+    Returns individual shapes
+    """
+    gamma_shape = ensemble.shape[1]
+    dimensions = observations.shape[0]
+    return gamma_shape, dimensions
+
+
+def _convergence_non_vec(ensemble_size, u_exact, r, e, m1, gamma, g, p,
+                         misfit):
+    """
+    Non vectorized convergence function
+    """
+    tmp_e = 0
+    tmp_r = 0
+    tmp_ae = 0
+    tmp_ar = 0
+    tmp_m = 0
+    for l in range(ensemble_size):
+        tmp_r += norm(r[:, l], 2) ** 2 / norm(u_exact, 2) ** 2
+        tmp_e += norm(e[:, l], 2) ** 2 / norm(m1, 2) ** 2
+        tmp_ae += sqrt(inv(gamma)) @ g @ e[:, l].T @ \
+                  sqrt(inv(gamma)) @ g @ e[:, l] / norm(g @ m1) ** 2
+        tmp_ar += sqrt(inv(gamma)) @ g @ r[:, l].conj().T @ \
+                  sqrt(inv(gamma)) @ g @ r[:, l] / norm(p) ** 2
+        tmp_m += norm(misfit[:, l], 2) ** 2
+    return tmp_e, tmp_r, tmp_ae, tmp_ar, tmp_m
+
+
+def _stopping_criterion(y, y_hat, loss_function='BCE'):
+    """
+    Loss functions
+    :param y: target
+    :param y_hat: calculated output (here G(u) or feed-forword output a)
+    :param loss_function: name of the loss function
+    :return: cost calculated according to `loss_function`
+    """
+    if loss_function == 'BCE':
+        term1 = -y * np.log(y_hat)
+        term2 = (1 - y) * np.log(1 - y_hat)
+        return np.sum(term1 - term2)
+    elif loss_function == 'MAE' or 'L1':
+        return np.sum(np.absolute(y_hat - y))
+    elif loss_function == 'MSE' or 'L2':
+        return np.sum((y_hat - y) ** 2 / y.size)
+    else:
+        raise KeyError(
+            'Loss Function \'{}\' not understood.'.format(loss_function))
+
+
+def _flatten_to_net_weights(model, flattened_weights):
+    weight_shapes = model.get_weights_shapes()
+
+    cumulative_num_weights_per_layer = np.cumsum(
+        [np.prod(weight_shape) for weight_shape in weight_shapes])
+
+    weights = []
+    for i, weight_shape in enumerate(weight_shapes):
+        if i == 0:
+            w = flattened_weights[
+                :cumulative_num_weights_per_layer[i]].reshape(weight_shape)
+        else:
+            w = flattened_weights[
+                cumulative_num_weights_per_layer[i - 1]:
+                cumulative_num_weights_per_layer[i]].reshape(weight_shape)
+        weights.append(w)
+    return weights
+
+
+def _calculate_misfit(ensemble, ensemble_size, dims, misfit, r, g_all, u_exact,
+                      noise):
+    """
+    Calculates and returns the misfit and the deviation from the true solution
+    """
+    for d in range(dims):
+        if dims >= 2:
+            g = g_all[d]
+        else:
+            g = g_all
+        for l in range(ensemble_size):
+            r[:, l] = ensemble[:, l] - u_exact
+            misfit[:, l] = g[:, l] * r[l, 0] - noise
+    return misfit, r
