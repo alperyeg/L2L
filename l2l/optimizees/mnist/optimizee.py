added covariance shifting to the sampling with exp. decay, fixed a small bug

l2l/optimizees/mnist/optimizee.py
@@ -91,13 +91,10 @@ class MNISTOptimizee(Optimizee):
                     targets=self.data_targets,
                     input=self.data_images)
 
-    def _create_individual_distribution(self, random_state, weights):
-        # cov_mat = np.cov(weights)
-        # mean = np.mean(weights, axis=0)
-        # new_individual = np.random.multivariate_normal(mean, cov_mat)
+    def _create_individual_distribution(self, random_state, weights, epsilon=0):
         dist = distribution.Gaussian()
         dist.init_random_state(random_state)
-        dist.fit(weights)
+        dist.fit(weights, epsilon)
         new_individuals = dist.sample(self.n_ensembles)
         return new_individuals
 

l2l/optimizers/crossentropy/distribution.py
@@ -73,13 +73,16 @@ class Gaussian(Distribution):
         params_dict_items = [("distribution_name", self.__class__.__name__)]
         return dict(params_dict_items)
 
-    def fit(self, data_list, smooth_update=0):
+    def fit(self, data_list, smooth_update=0, epsilon=0.0):
         """
         Fit a gaussian distribution to the given data
 
         :param data_list: list or numpy array with individuals as rows
         :param smooth_update: determines to which extent the new samples account for the new distribution.
           default is 0 -> old parameters are fully discarded
+        :param epsilon: float, A non-zero value is added on the diagonal of the
+            covariance matrix, to spread the distribution to explore
+            other local minima.
         
         :return dict: specifying current parametrization
         """
@@ -96,6 +99,9 @@ class Gaussian(Distribution):
 
         self.mean = smooth_update * self.mean + (1 - smooth_update) * mean
         self.cov = smooth_update * self.cov + (1 - smooth_update) * cov_mat
+
+        if epsilon != 0.0:
+            self.cov += epsilon * np.eye(self.cov.shape[0])
 
         logger.debug('Gaussian center\n%s', self.mean)
         logger.debug('Gaussian cov\n%s', self.cov)

l2l/optimizers/kalmanfilter/optimizer.py
@@ -14,7 +14,7 @@ EnsembleKalmanFilterParameters = namedtuple(
     'EnsembleKalmanFilter', ['noise', 'gamma', 'tol',
                              'maxit', 'stopping_crit', 'n_iteration',
                              'pop_size', 'shuffle', 'n_batches', 'online',
-                             'seed']
+                             'epsilon', 'decay_rate', 'seed']
 )
 
 
@@ -32,6 +32,18 @@ EnsembleKalmanFilterParameters.__doc__ = """
 :param n_batches: int, Number of mini-batches to use in the Kalman Filter
 :param online: bool, Indicates if only one data point will used, 
                Default: False
+:param epsilon: float, A value which is used when sampling from the best individual. 
+                The value is multiplied to the covariance matrix as follows:
+                :math:`\\epsilon * I` where I is the identity matrix with the 
+                same size as the covariance matrix. The value is 
+                exponentially decaying and should be in [0,1] and used in 
+                combination with `decay_rate`. 
+:param decay_rate: float, Decay rate for the sampling. 
+                For the exponential decay as follows:
+                .. math::
+                    \\epsilon = \\epsilon_0 e^{-decay_rate * epoch}
+                    
+                Where :math:`\\epsilon` is the value from `epsilon`. The
 :param seed: The random seed used to sample and fit the distribution. 
              Uses a random generator seeded with this seed.
 """
@@ -74,6 +86,8 @@ class EnsembleKalmanFilter(Optimizer):
         traj.f_add_parameter('shuffle', parameters.shuffle)
         traj.f_add_parameter('n_batches', parameters.n_batches)
         traj.f_add_parameter('online', parameters.online)
+        traj.f_add_parameter('epsilon', parameters.epsilon)
+        traj.f_add_parameter('decay_rate', parameters.decay_rate)
         traj.f_add_parameter('seed', np.uint32(parameters.seed),
                              comment='Seed used for random number generation '
                                      'in optimizer')
@@ -89,6 +103,12 @@ class EnsembleKalmanFilter(Optimizer):
 
         #: The current generation number
         self.g = 0
+        # for the sampling procedure
+        # `epsilon` value given by the user
+        self.epsilon = parameters.epsilon
+        # decay rate
+        self.decay_rate = parameters.decay_rate
+
         #: The population (i.e. list of individuals) to be evaluated at the
         # next iteration
         current_eval_pop = [self.optimizee_create_individual() for _ in range(parameters.pop_size)]
@@ -141,24 +161,21 @@ class EnsembleKalmanFilter(Optimizer):
             # get the score/fitness of the individual
             fitness_per_individual = traj.current_results[i.ind_idx][1][0]
             fitnesses.append(fitness_per_individual)
-            if self.g > 1 and self.g % 1000 == 0:
-                continue
-            else:
-                model_output = traj.current_results[i.ind_idx][1][1]
-                new_ensembles = update_enknf(data=data_input[0:100],
-                                             ensemble=ens,
-                                             ensemble_size=ensemble_size,
-                                             moments1=np.mean(ens, axis=0),
-                                             u_exact=None,
-                                             observations=data_targets[0:100],
-                                             model_output=model_output,
-                                             noise=traj.noise,
-                                             p=None, gamma=gamma, tol=traj.tol,
-                                             maxit=traj.maxit,
-                                             stopping_crit=traj.stopping_crit,
-                                             online=traj.online,
-                                             shuffle=traj.shuffle)
-                all_results.append(new_ensembles[0])
+            model_output = traj.current_results[i.ind_idx][1][1]
+            new_ensembles = update_enknf(data=data_input[0:100],
+                                         ensemble=ens,
+                                         ensemble_size=ensemble_size,
+                                         moments1=np.mean(ens, axis=0),
+                                         u_exact=None,
+                                         observations=data_targets[0:100],
+                                         model_output=model_output,
+                                         noise=traj.noise,
+                                         p=None, gamma=gamma, tol=traj.tol,
+                                         maxit=traj.maxit,
+                                         stopping_crit=traj.stopping_crit,
+                                         online=traj.online,
+                                         shuffle=traj.shuffle)
+            all_results.append(new_ensembles[0])
 
         generation_name = 'generation_{}'.format(self.g)
         traj.results.generation_params.f_add_result_group(generation_name)
@@ -170,10 +187,14 @@ class EnsembleKalmanFilter(Optimizer):
             self.best_fitness = best_fitness
             best_ranking_idx = ranking_idx[0]
             self.best_individual = individuals[best_ranking_idx]
+            # do the decay
+            eps = self.epsilon * np.exp(-self.decay_rate * self.g)
+            # now do the sampling
             shifts = [
                 self.optimizee_create_new_individuals(self.random_state,
                                                       individuals[
-                                                          best_ranking_idx].shift)
+                                                          best_ranking_idx].shift,
+                                                      eps)
                 for _ in range(traj.pop_size)]
 
             self.eval_pop = [dict(shift=shifts[i],

l2l/optimizers/kalmanfilter/updateEnKF.py
@@ -82,7 +82,7 @@ def update_enknf(data, ensemble, ensemble_size, moments1, u_exact,
     if isinstance(gamma, (int, float)):
         if float(gamma) == 0.:
             gamma = np.eye(gamma_s)
-    # sqrt_inv_gamma = sqrt(inv(gamma))
+    sqrt_inv_gamma = sqrt(inv(gamma))
 
     Cpp = None
     Cup = None
@@ -95,15 +95,18 @@ def update_enknf(data, ensemble, ensemble_size, moments1, u_exact,
 
     total_cost = []
     for i in range(maxit):
+        if (i % 100) == 0:
+            print('Iteration {}/{}'.format(i, maxit))
         if shuffle:
             data, observations = _shuffle(data, observations)
 
         # check for early stopping
-        # _convergence(norm_uexact2=norm_uexact2,
-        #              norm_p2=norm_p2,
-        #              sqrt_inv_gamma=sqrt_inv_gamma,
-        #              ensemble_size=ensemble_size, G=g_all, m1=m1, M=M, E=E,
-        #              R=R, AE=AE, AR=AR, e=e, r=r, misfit=misfit)
+        _convergence(norm_uexact2=norm_uexact2,
+                     norm_p2=norm_p2,
+                     sqrt_inv_gamma=sqrt_inv_gamma,
+                     ensemble_size=ensemble_size, G=model_output, m1=m1, M=M,
+                     E=E,
+                     R=R, AE=AE, AR=AR, e=e, r=r, misfit=misfit)
         if stopping_crit == 'discrepancy' and noise > 0:
             if M[i] <= np.linalg.norm(noise, 2) ** 2:
                 break
@@ -134,31 +137,32 @@ def update_enknf(data, ensemble, ensemble_size, moments1, u_exact,
             for l in range(ensemble_size):
                 e[l] = ensemble[l] - m1
             if u_exact is not None:
-                misfit, r = _calculate_misfit(ensemble, ensemble_size, dims,
-                                              misfit, r, model_output
-                                              , u_exact, noise)
+                misfit[:, :, idx], r = _calculate_misfit(ensemble,
+                                                         ensemble_size,
+                                                         misfit[:, :, idx], r,
+                                                         model_output[:,
+                                                                      :, idx],
+                                                         u_exact, noise)
+            # in case of online learning idx should be an int
+            # put it into a list to loop over it
+            if not isinstance(idx, np.ndarray):
+                idx = [idx]
 
-            if not online:
-                for d in range(len(idx)):
-                    # reshape first the model_output
-                    g = model_output.reshape(-1, dims, gamma_s)
-                    # now get only the individuals output according to idx
-                    g = g[idx][d]
-                    ensemble = _update_step(ensemble, observations[idx], g,
-                                            gamma, ensemble_size, d)
-            else:
-                g = model_output.T
-                ensemble = _update_step(ensemble, observations, g, gamma,
-                                        ensemble_size, idx)
+            # reshape first the model_output
+            # to ensembles x examples x output
+            # g = model_output.reshape(-1, dims, gamma_s)
+            for d in idx:
+                # now get only the individuals output according to idx
+                g_tmp = model_output[:, :, d]
+                ensemble = _update_step(ensemble, observations[d],
+                                        g_tmp, gamma, ensemble_size)
+
         m1 = np.mean(ensemble, axis=0)
-        if (i % 100) == 0:
-            print('Iteration {}/{}'.format(i, maxit))
-
     # return M, E, R, AE, AR, Cpp, Cup, m1
     return ensemble, Cpp, Cup, total_cost
 
 
-def _update_step(ensemble, observations, g,  gamma, ensemble_size, idx):
+def _update_step(ensemble, observations, g,  gamma, ensemble_size):
     """
     Update step of the kalman filter
     Calculates the covariances and returns new ensembles
@@ -168,7 +172,7 @@ def _update_step(ensemble, observations, g,  gamma, ensemble_size, idx):
     Cpp = _cov_mat(g, g, ensemble_size)
     for j in range(ensemble_size):
         # create one hot vector
-        target = observations[idx]
+        target = observations
         tmp = solve(Cpp + gamma, target - g[j])
         ensemble[j] = ensemble[j] + (Cup @ tmp)
     return ensemble
@@ -196,7 +200,7 @@ def _cov_mat(x, y, ensemble_size):
     x_bar = _get_mean(x)
     y_bar = _get_mean(y)
 
-    cov = np.zeros((x.shape[1], y.shape[1]))
+    cov = 0.0
     for j in range(ensemble_size):
         try:
             cov = cov + np.tensordot((x[j] - x_bar), (y[j] - y_bar).T, 0)
@@ -312,19 +316,21 @@ def _flatten_to_net_weights(model, flattened_weights):
     return weights
 
 
-def _calculate_misfit(ensemble, ensemble_size, dims, misfit, r, g_all, u_exact,
+def _calculate_misfit(ensemble, ensemble_size, misfit, r, g_all, u_exact,
                       noise):
     """
     Calculates and returns the misfit and the deviation from the true solution
     """
-    for d in range(dims):
-        if dims >= 2:
-            g = g_all[d]
-        else:
-            g = g_all
-        for l in range(ensemble_size):
-            r[:, l] = ensemble[:, l] - u_exact
-            misfit[:, l] = g[:, l] * r[l, 0] - noise
+    for d in range(ensemble_size):
+        r[d] = ensemble[d] - u_exact
+        misfit[d] = g_all[d] * r[d, 0] - noise
+        # if len(g_all) >= 2:
+        #     g = g_all[d]
+        # else:
+        #     g = g_all
+        # for l in range(ensemble_size):
+        #     r[l] = ensemble[l] - u_exact
+        #     misfit[d][l] = g[l] * r[l, 0] - noise
     return misfit, r
 
 